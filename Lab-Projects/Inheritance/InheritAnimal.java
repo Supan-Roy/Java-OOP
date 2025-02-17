51e71e2e4cf4423559dde18af4c3566c2dbd2251
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-02-18 00:36:33 +0600

    Add files via upload

---
+++ Lab-Projects/Inheritance/InheritAnimal.java
@@ -0,0 +1,44 @@
+/**
+ *
+ * @author supan
+ */
+class Animal{
+    String name;
+    double weight;
+    
+  Animal(String name, double weight){
+        this.name = name;
+        this.weight = weight;
+    }
+    
+    void eat(){
+        System.out.println(name + " is eating");
+    }
+}
+
+class Dog extends Animal{
+    String breed;
+    
+    Dog(String name, double weight, String breed){
+        super(name,weight);
+        this.breed = breed;
+}
+    @Override
+    void eat(){
+        System.out.println(name + " is eating food");
+    }
+    void bark(){
+        System.out.println(name + " is barking");
+    }
+}
+public class InheritAnimal {
+    public static void main(String[] args) {
+        //Object of the Dog class
+        Dog d = new Dog("Buddy", 20.5, "Golden Retriever");
+        
+        //Access inherited method
+        d.eat();
+        //Access subclass-specific method
+        d.bark();
+    }
+}
