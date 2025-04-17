28540234a5c06ea02cb019bf394f99f0f6d1256b
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-04-17 23:32:50 +0600

    Update Abstraction.java

--- OOP221/Abstraction.java
+++ OOP221/Abstraction.java
@@ -22,7 +22,7 @@ class Dog extends Animal {
 }
 public class Test {
     public static void main(String[] args) {
-        Dog d1 = new Dog("Tommy");
+        Dog d1 = new Dog("Hound");
         d1.makeSound(); 
         d1.sleep();    
     }
